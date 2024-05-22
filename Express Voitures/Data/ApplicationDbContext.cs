01c25b3de65ffdaffa3920c028da9059eb523f66
Elodie Caradeuc <caradeuc.elodie@gmail.com>
2024-05-22 11:01:47 +0200

    Ajoutez des fichiers projet.

---
+++ Express Voitures/Data/ApplicationDbContext.cs
@@ -0,0 +1,50 @@
+﻿using ExpressVoitures.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext<User>
+{
+    public DbSet<Car> Cars { get; set; }
+    public DbSet<Repair> Repairs { get; set; }
+    public DbSet<Sale> Sales { get; set; }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Configuration des clés primaires et des relations
+        modelBuilder.Entity<Car>()
+            .HasKey(v => v.VIN);
+
+        modelBuilder.Entity<Repair>()
+            .HasKey(r => r.RepairId);
+
+        modelBuilder.Entity<Repair>()
+            .HasOne(r => r.Car)
+            .WithMany(v => v.Repairs)
+            .HasForeignKey(r => r.VIN);
+
+        modelBuilder.Entity<Repair>()
+            .HasOne(r => r.User)
+            .WithMany(u => u.Repairs)
+            .HasForeignKey(r => r.UserId);
+
+        modelBuilder.Entity<Sale>()
+            .HasKey(s => s.SaleId);
+
+        modelBuilder.Entity<Sale>()
+            .HasOne(s => s.Car)
+            .WithMany(v => v.Sales)
+            .HasForeignKey(s => s.VIN);
+
+        modelBuilder.Entity<Sale>()
+            .HasOne(s => s.User)
+            .WithMany(u => u.Sales)
+            .HasForeignKey(s => s.UserId);
+    }
+}
---
+++ Express Voitures/Data/Sale.cs
@@ -0,0 +1,24 @@
+﻿using ExpressVoitures.Data;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Sale
+{
+    [Key]
+    public int SaleId { get; set; }
+
+    [ForeignKey("Car")]
+    public required string VIN { get; set; }
+
+    public required Car Car { get; set; }
+
+    public decimal SalePrice { get; set; }
+
+    public DateTime SaleDate { get; set; }
+
+    [ForeignKey("User")]
+    public required string UserId { get; set; }
+
+    public required User User { get; set; }
+}
